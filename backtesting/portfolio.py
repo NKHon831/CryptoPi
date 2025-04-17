42fe19ce273df1f3e3d9d2733e3cc96c204f3c88
NKHon831 <khaihon030831@gmail.com>
2025-04-17 16:41:08 +0800

    clean: comment out unrelated code

--- backtesting/portfolio.py
+++ backtesting/portfolio.py
@@ -1,11 +1,13 @@
-class Portfolio:
-    def __init__(self, holdings = 0.0, cash = 0.0):
-        self.pending_orders = []
-        self.filled_orders = []
-        self.active_trades = []
-        self.closed_trades = []
-        self.performance = 
-        self.position = 
-        self.holdings = holdings
-        self.cash = cash
-        self.
+# class Portfolio:
+#     def __init__(self, holdings = 0.0, cash = 0.0):
+#         self.pending_orders = []
+#         self.filled_orders = []
+#         self.active_trades = []
+#         self.closed_trades = []
+#         self.performance = 
+#         self.position = 
+#         self.holdings = holdings
+#         self.cash = cash
+#         self.
+
+# will do it in other branch
