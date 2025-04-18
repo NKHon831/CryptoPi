c56d36e97a81a0fecbc6286819404a17471944fd
vanessajing <roboticsnrc2019@gmail.com>
2025-04-18 18:10:58 +0800

    Update gitignore

--- regime_detection/config.py
+++ regime_detection/config.py
@@ -1,6 +1,6 @@
-INPUT_CSV_PATH = r"C:\\Users\\riche\\OneDrive\\Documents\\UMH25\\Input\\BTC-USD_data.csv"
+INPUT_CSV_PATH = r".\\Input\\BTC-USD_data.csv"
 OUTPUT_CSV_PATH = r".\\output\\regime_csv"
-OUTPUT_JPG_PATH = r".\\output\\regime_graph"
+# OUTPUT_JPG_PATH = r".\\output\\regime_graph"
 N_HMM_STATES = 3
 HMM_FEATURES = ["log_returns", "volatility_10", "RSI_14", "MACD", "ATR_14", "OBV"]
 MODEL_PATH_PREFIX = r".\\models\\btc_hmm\\btc"
